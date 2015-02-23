011d02a744bf45a87f67b14d7cfbbf7e52cba1c4
Kulkulcan <kulkulkan-github@sarumino.com>
2015-02-23 09:54:16 -0700

    input cleanup

--- classes/commands/command_factory.rb
+++ classes/commands/command_factory.rb
@@ -3,7 +3,12 @@ class CommandFactory
   def self.parse_input_string str
     $player.debug_output 'Trying to figure out what you want when you say, "' + str + '."', 2
 
-    words = (str.gsub /[^a-z0-9 ]+/, ' ').split(' ')
+    str.gsub! /[^a-z0-9 ]+/, ' '
+    str.strip!
+
+    return nil unless str.length > 0
+
+    words = str.split(' ')
     words.map { | word | word.strip! }
 
     first_word = words[0]
