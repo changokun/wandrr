bb5d89a3362b101da33c75e6fd7319487088ad64
Kulkulcan <kulkulkan-github@sarumino.com>
2015-02-23 09:52:03 -0700

    kill needs DO

--- classes/commands/kill_command.rb
+++ classes/commands/kill_command.rb
@@ -1,8 +1,12 @@
-class KillCommand < Command
+require 'non_reflexive_command'
+
+class KillCommand < NonReflexiveCommand
+
   def execute
-    @game.player.debug_output self.class.to_s
+    $player.debug_output self.class.to_s
     puts 'With what, your bare hands?'
   end
+  
 end
 
 FightCommand = Class.new(KillCommand)
--- classes/non_reflexive_command.rb
+++ classes/non_reflexive_command.rb
@@ -8,9 +8,13 @@ class NonReflexiveCommand < Command
     @direct_object = nil
 
     # is one of the words an object that can be looked at?
+    verb = words.shift
+    $player.debug_output 'What would Player like to ' + verb.red + '?'
     words.each do | word |
-      $player.debug_output 'Is there a(n) ' + word + ' to do something to/at/on?'
+      $player.debug_output 'Is there a(n) ' + word + ' to ' + verb.red + '?'
     end
+
+    puts 'We need to figure out how to get a direct object for your command, but we will roll without one for now'.green
 
   end
 
