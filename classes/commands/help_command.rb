5336d60fae357ca0d0e8407222957d6c8d725f95
Kulkulcan <kulkulkan-github@sarumino.com>
2015-02-28 11:58:27 -0700

    expanded help

--- classes/commands/help_command.rb
+++ classes/commands/help_command.rb
@@ -7,14 +7,18 @@ class HelpCommand < SystemCommand
     puts 'HELP MENU'
     puts 'Maybe we will loop through the base command classes, and group the system, game, and secret commands here.'
     puts 'but for now you can:'
+
     puts "\n------ Player Commands".green
-    puts 'look - for now will describe your surroundings.'
-    puts 'wait - pass some time. Let others act.'
-    puts 'die - end the game.'
+    puts 'look [something] - will describe your surroundings unless you look at something.'
+    puts 'wait, pass - pass some time. Let others act.'
+    puts 'die, exit, quit, q - end the game.'
+    puts 'push, pull, move - general interactions.'
+
     puts "\n------ System Commands".green
     puts 'save - does nothing.'
     puts 'load - does nothing.'
     puts 'set - will toggle debug output.'
+
     puts "\n------ Secret Commands".green
     puts 'burp'
     puts "\n------------------------------\n".green
