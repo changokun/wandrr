0f87c0a0b89c52262a8e86eee556faa9a3b8b5f2
Matt Morris <matt.morris@placewise.com>
2015-03-07 11:25:19 -0700

    clean up command_factory a bit

--- classes/commands/command_factory.rb
+++ classes/commands/command_factory.rb
@@ -1,23 +1,15 @@
 class CommandFactory
-
   def self.parse_input_string str
     $player.debug_output %Q(Trying to figure out what you want when you say, "#{str}."), 2
 
     return nil unless str.length > 0
 
-    words = str.split(' ')
-    words.map(&:strip!)
-
-    first_word = words[0]
-
-    potential_command_class_name = first_word.capitalize + 'Command'
+    words = str.split(/\s+/)
+    potential_command_class_name = "#{words.first.capitalize}Command"
 
     return nil unless Object.const_defined?(potential_command_class_name)
 
     command_class = Object.const_get potential_command_class_name
-
     command_class.new words
-
   end
-
 end
