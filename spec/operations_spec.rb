4421fdfe62cd8353afbfe1b0c8421c6d52b791a8
Raymond <github+raymond@sarumino.com>
2015-05-09 10:15:45 -0600

    adding rspec, but it is/I am currently useless

---
+++ spec/operations_spec.rb
@@ -0,0 +1,12 @@
+wandrr_root = File.expand_path('../', File.dirname(__FILE__))
+
+
+puts "\nI'm having a hard time putting together any tests at this time. will come back to this later.\n"
+
+=begin
+  it "should create data folder if it does not exist" do
+    expect(File).not_to exist("#{wandrr_root}/public/pdf")
+  end
+
+=end
+
